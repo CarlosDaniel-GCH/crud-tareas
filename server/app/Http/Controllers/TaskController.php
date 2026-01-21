d7ea6a0114998eb92831546faa15e0d0474b1d39
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-21 11:05:58 -0500

    Metodo actualizar y eliminar tareas

--- server/app/Http/Controllers/TaskController.php
+++ server/app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
@@ -23,7 +24,7 @@ class TaskController extends Controller
 
     public function store(Request $request){
         $validate = $request->validate([
-            'titulo' => 'required|string',
+            'titulo' => 'required|string|max:255',
             'descripcion' => 'nullable|string',
             'completed' => 'required|boolean'
         ]);
@@ -37,5 +38,50 @@ class TaskController extends Controller
         }
 
         return Task::create($validate);
+    }
+
+    public function update(Request $request, $id){
+        $task = Task::find($id);
+
+        if(!$task){
+            return response()->json('Tarea no encontrada.', 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'completed' => 'required|boolean'
+        ]);
+
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error en los tipos de datos',
+                'errors' => $validator->errors()
+            ];
+
+            return response()->json($data);
+        }
+
+        if($request->has('completed')){
+            $task->completed = $request->completed;
+        }
+
+        $task->save();
+
+        $data = [
+            'message' => "Tarea actualizada"
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id){
+        $task = Task::find($id);
+
+        if(!$task){
+            return response()->json('Tarea no encontrada', 404);
+        }
+
+        $task->delete();
+
+        return response()->json("Tarea eliminada",200);
     }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
+Route::patch('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}',[TaskController::class, 'destroy']);
